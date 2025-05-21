907f39a50e93e05fc55a8a473892410a86c4e0c7
nicepass <50690714+nicepass@users.noreply.github.com>
2025-05-21 09:14:11 +0800

    添加CPE插件

--- diy-script.sh
+++ diy-script.sh
@@ -54,6 +54,9 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
+# CPE插件
+git clone --depth=1 https://github.com/FUjr/QModem package/QModem
+
 # 更改 Argon 主题背景
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
